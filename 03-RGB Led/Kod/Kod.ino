96d511a06b03ad9a416ae4aff1ea8b718abb6e3f
Onur <108219299+T7GR4L@users.noreply.github.com>
2026-02-22 18:44:19 +0300

    Add files via upload

---
+++ 03-RGB Led/Kod/Kod.ino
@@ -0,0 +1,34 @@
+void setup()
+{
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(9, HIGH);
+  delay(3000); // Wait for 3000 millisecond(s)
+  digitalWrite(9, LOW);
+  digitalWrite(10, HIGH);
+  delay(3000); // Wait for 3000 millisecond(s)
+  digitalWrite(10, LOW);
+  digitalWrite(11, HIGH);
+  delay(3000); // Wait for 3000 millisecond(s)
+  digitalWrite(11, LOW);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, HIGH);
+  delay(3000); // Wait for 3000 millisecond(s)
+  digitalWrite(10, LOW);
+  digitalWrite(11, HIGH);
+  delay(3000); // Wait for 3000 millisecond(s)
+  digitalWrite(9, LOW);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, HIGH);
+  delay(3000); // Wait for 3000 millisecond(s)
+  digitalWrite(9, HIGH);
+  delay(3000); // Wait for 3000 millisecond(s)
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+}
