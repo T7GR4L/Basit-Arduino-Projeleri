7ab47b088e7a4a161e5cdcdcdb32e235b2fe3c76
Onur <108219299+T7GR4L@users.noreply.github.com>
2026-02-28 17:19:30 +0300

    Add files via upload

---
+++ 11-Park Sensörü/Kod/Kod.ino
@@ -0,0 +1,26 @@
+long readUltrasonicDistance(int triggerPin, int echoPin)
+{
+  pinMode(triggerPin, OUTPUT);  // Clear the trigger
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigger pin to HIGH state for 10 microseconds
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  pinMode(echoPin, INPUT);
+  // Reads the echo pin, and returns the sound wave travel time in microseconds
+  return pulseIn(echoPin, HIGH);
+}
+
+void setup()
+{
+  pinMode(5, OUTPUT);
+}
+
+void loop()
+{
+  while (20 > 0.01723 * readUltrasonicDistance(4, 3)) {
+    tone(5, 523, 100); // play tone 60 (C5 = 523 Hz)
+    delay((3 * 0.01723 * readUltrasonicDistance(4, 3))); // Wait for (2 * 0.01723 * readUltrasonicDistance(4, 3)) millisecond(s)
+  }
+}
